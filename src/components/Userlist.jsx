26975b59d7918189d2c3ae9d70e86748101f3b75
jeedddiiii <pendigon.once@gmail.com>
2024-03-13 21:23:02 +0700

    Update get user

--- src/components/Userlist.jsx
+++ src/components/Userlist.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 function Userlist() {
   // Sample user data
-  const users = [
-    { id: 1, username: "SWK" },
-    { id: 2, username: "SRS" },
-    { id: 3, username: "KYS" },
-  ];
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/") // Replace with your API endpoint
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
 
   return (
     <div className="">
@@ -20,21 +22,42 @@ function Userlist() {
           </tr>
         </thead>
         <tbody>
-          {users.map((user) => (
-            <tr>
-              <th scope="row">{user.id}</th>
-              <td>{user.username}</td>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <th scope="row">{index+1}</th>
+              <td>{user.name}</td>
               <td>
                 <button
                   className="btn mx-1"
                   onClick={() => handleEdit(user.id)}
                 >
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M3 21v-4.25L16.2 3.575q.3-.275.663-.425t.762-.15q.4 0 .775.15t.65.45L20.425 5q.3.275.438.65T21 6.4q0 .4-.137.763t-.438.662L7.25 21zM17.6 7.8L19 6.4L17.6 5l-1.4 1.4z"/></svg>                </button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M3 21v-4.25L16.2 3.575q.3-.275.663-.425t.762-.15q.4 0 .775.15t.65.45L20.425 5q.3.275.438.65T21 6.4q0 .4-.137.763t-.438.662L7.25 21zM17.6 7.8L19 6.4L17.6 5l-1.4 1.4z"
+                    />
+                  </svg>{" "}
+                </button>
                 <button
                   className="btn mx-1"
                   onClick={() => handleDelete(user.id)}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zM9 17h2V8H9zm4 0h2V8h-2zM7 6v13z"/></svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zM9 17h2V8H9zm4 0h2V8h-2zM7 6v13z"
+                    />
+                  </svg>
                 </button>
               </td>
             </tr>
