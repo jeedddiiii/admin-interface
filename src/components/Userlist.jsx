86d1d76cee420cb430e6dc2af8fe5b89583a831f
jeedddiiii <pendigon.once@gmail.com>
2024-03-27 15:20:24 +0700

    Add modal

--- src/components/Userlist.jsx
+++ src/components/Userlist.jsx
@@ -1,25 +1,31 @@
 import React, { useEffect, useState } from "react";
 
-function Userlist({searchName}) {
+function Userlist({ searchName }) {
   const [users, setUsers] = useState([]);
-  const filteredUsers = users.filter(user =>searchName ? user.name ===searchName : true);
+  const [userToDelete, setUserToDelete] = useState(null);
+  const filteredUsers = users.filter((user) =>
+    searchName ? user.name === searchName : true
+  );
 
   useEffect(() => {
-    fetch("http://localhost:8080/") 
+    fetch("http://localhost:8080/")
       .then((response) => response.json())
       .then((data) => setUsers(data));
   }, []);
 
   const handleDelete = (id) => {
-    fetch(`http://localhost:8080/delete-user/${id}`, { 
-      method: 'DELETE',
+    fetch(`http://localhost:8080/delete-user/${id}`, {
+      method: "DELETE",
     })
-    .then(() => {
-      setUsers(users.filter(user => user.id !== id));
-    })
-    .catch((error) => console.error('Error:', error));
+      .then(() => {
+        setUsers(users.filter((user) => user.id !== id));
+      })
+      .catch((error) => console.error("Error:", error));
   };
 
+  const handleDeleteModal = (user) => {
+    setUserToDelete(user);
+  };
 
   return (
     <div className="">
@@ -34,7 +40,7 @@ function Userlist({searchName}) {
         <tbody>
           {filteredUsers.map((user, index) => (
             <tr key={index}>
-              <th scope="row">{index+1}</th>
+              <th scope="row">{index + 1}</th>
               <td>{user.name}</td>
               <td>
                 <button
@@ -54,8 +60,10 @@ function Userlist({searchName}) {
                   </svg>{" "}
                 </button>
                 <button
-                  className="btn mx-1"
-                  onClick={() => handleDelete(user.id)}
+                  className="btn mx-1 "
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                  onClick={() => handleDeleteModal(user)}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -74,6 +82,48 @@ function Userlist({searchName}) {
           ))}
         </tbody>
       </table>
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
+                Delete User
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+            Are you sure you want to delete {userToDelete && userToDelete.name}?
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Cancel
+              </button>
+              <button type="button" data-bs-dismiss="modal" class="btn btn-danger" onClick={() => {
+                  handleDelete(userToDelete.id);
+                  setUserToDelete(null); // Reset user data for deletion
+                }}
+>
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
